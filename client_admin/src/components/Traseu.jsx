fb63128776787dc9b4ce987aa2a5c1927f982992
Tudor-Bondoc <tudor.bondoc2012@gmail.com>
2024-01-10 20:47:10 +0200

    Added first commit of client admin

---
+++ client_admin/src/components/Traseu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Traseu.css"
+
+export default function Traseu(props){
+    return(
+        <div className="traseu--container" /*onClick={() => props.onClick()}*/>
+            <h1 className="oras">Oras pornire: {props.oras_pornire}</h1>
+            {props.oras_1 && <h1 className="oras">Prima oprire: {props.oras_1}</h1>}
+            {props.oras_2 && <h1 className="oras">A doua oprire: {props.oras_2}</h1>}
+            {props.oras_3 && <h1 className="oras">A treia oprire: {props.oras_3}</h1>}
+            <h1 className="oras">Oras destinatie: {props.oras_sosire}</h1>
+        </div>
+    )
+}
