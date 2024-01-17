c55ad27a7eaabd57f09dd83df8bc57feec1ad69e
Tudor-Bondoc <tudor.bondoc2012@gmail.com>
2024-01-17 20:07:08 +0200

    implemented routes

--- client_admin/src/App.jsx
+++ client_admin/src/App.jsx
@@ -1,40 +1,17 @@
 import React from "react";
-import axios from "axios"
-import Traseu from "./components/Traseu";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 import Header from "./components/Header";
+import Trasee from "./pages/Trasee";
 
 export default function App() {
-
-  const [listaTrasee, setListaTrasee] = React.useState([])
-
-  //Mapare lista de trasee catre componenta Traseu
-  const lista = listaTrasee.map((value, key) => {
-    return(
-      <Traseu
-        key = {value.id}
-        oras_pornire = {value.oras_pornire}
-        oras_1 = {value.oras_1}
-        oras_2 = {value.oras_2}
-        oras_3 = {value.oras_3}
-        oras_sosire = {value.oras_sosire}
-        /*onClick = {()=>{navigate(`/post/${value.id}`)}}*/
-      />
-    )
-  }) 
-
-  //Request catre API pentru lista de trasee
-  React.useEffect(()=> {
-    axios.get("http://localhost:3002/trasee").then((response)=> {
-      setListaTrasee(response.data)
-    })
-  }, [])
-
-
-
   return(
     <div>
-      <Header />
-      {lista}
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Trasee />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
---
+++ client_admin/src/pages/Trasee.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import axios from "axios"
+import Traseu from "../components/Traseu";
+
+export default function Trasee(){
+
+    const [listaTrasee, setListaTrasee] = React.useState([])
+
+    //Mapare lista de trasee catre componenta Traseu
+    const lista = listaTrasee.map((value, key) => {
+    return(
+      <Traseu
+        key = {value.id}
+        oras_pornire = {value.oras_pornire}
+        oras_1 = {value.oras_1}
+        oras_2 = {value.oras_2}
+        oras_3 = {value.oras_3}
+        oras_sosire = {value.oras_sosire}
+        /*onClick = {()=>{navigate(`/post/${value.id}`)}}*/
+      />
+    )
+    }) 
+
+    //Request catre API pentru lista de trasee
+    React.useEffect(()=> {
+        axios.get("http://localhost:3002/trasee").then((response)=> {
+            setListaTrasee(response.data)
+        })
+    }, [])
+
+    return(
+        <div>
+            {lista}
+        </div>
+    )
+}
