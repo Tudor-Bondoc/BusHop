f6a08ab5dc17d178bd25af5c2c5d9f19f99342a4
Tudor-Bondoc <tudor.bondoc2012@gmail.com>
2024-03-02 21:10:12 +0200

    pasageri server route

---
+++ server/routes/Pasageri.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+const { Pasageri } = require("../models")
+const bcrypt = require("bcrypt")
+
+//Adauga un user
+router.post("/", async(req, res) => {
+    const { nume, email, parola, telefon } = req.body
+    bcrypt.hash(parola, 10).then((hash)=>{
+        Pasageri.create({
+            nume: nume,
+            email: email,
+            parola: hash,
+            telefon: telefon
+        })
+        res.json("SUCCESS")
+    })
+})
+
+router.post("/login", async(req, res) => {
+    const { email, parola } = req.body
+    const pasager = await Pasageri.findOne({
+        where: {
+            email: email
+        }
+    })
+
+    if (!pasager) res.json({error: "User does not exist"})
+
+    bcrypt.compare(parola, pasager.parola).then((match)=>{
+        if (!match) {
+            res.json({error: "Wrong username - password combination"})
+        }
+        res.json("You logged in")
+    })
+
+})
+
+
+
+module.exports = router
