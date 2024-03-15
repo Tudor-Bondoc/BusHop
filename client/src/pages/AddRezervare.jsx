895a90791ceb3c887b9f67beba76b5ce8892aec2
Tudor-Bondoc <tudor.bondoc2012@gmail.com>
2024-03-15 22:22:55 +0200

    added cursa details to rezervare page

--- client/src/pages/AddRezervare.jsx
+++ client/src/pages/AddRezervare.jsx
@@ -21,14 +21,36 @@ export default function AddRezervare() {
 
     const [numarScaunSelectat, setNumarScaunSelectat] = React.useState(null)
     const [listaRezervari, setListaRezervari] = React.useState([])
+    const [cursa, setCursa] = React.useState({})
+    const [listaTrasee, setListaTrasee] = React.useState([])
+    const [listaAutocare, setListaAutocare] = React.useState([])
+
 
     //Request API pentru lista de rezervari asociate Cursei id
     React.useEffect(()=>{
+
         axios.get(`http://localhost:3002/rezervari/${id}`).then((response)=> {
             setListaRezervari(response.data)
         })
+
+        axios.get(`http://localhost:3002/curse/${id}`).then((response)=>{
+            setCursa(response.data)
+        })
+
+        axios.get("http://localhost:3002/trasee").then((response)=> {
+            setListaTrasee(response.data)
+        })
+
+        axios.get("http://localhost:3002/autocare").then((response)=> {
+            setListaAutocare(response.data)
+        })
+
     }, [])
 
+    const traseuSelectat = listaTrasee.find(traseu => traseu.id === cursa.TraseuID)
+    const autocarSelectat = listaAutocare.find(autocar => autocar.id === cursa.AutocarID)
+    const ziPlecare = new Date(cursa.zi_plecare).toLocaleDateString();
+    
     let ocupate = []
 
     if (listaRezervari) {
@@ -77,17 +99,24 @@ export default function AddRezervare() {
     }
 
     return(
+
         <div className="add--rezervare--container">
 
         <div className="form--container--default2 form--modified">
             
                 <div className="form--rez">
+                    <p className="loc--selectat">Traseu: {traseuSelectat ? `${traseuSelectat.oras_pornire} - ${traseuSelectat.oras_sosire}` : 'Loading...'}</p>
+                    <p className="loc--selectat">Autocar: {autocarSelectat ? autocarSelectat.numar_inmatriculare : 'Loading...'}</p>
+                    <p className="loc--selectat">Zi plecare: {ziPlecare}</p>
+                    <p className="loc--selectat">Ora plecare: {cursa.ora_plecare}</p>
+                    <p className="loc--selectat">Ora sosire: {cursa.ora_sosire}</p>
                     <p className="loc--selectat">Nume pasager: {authState.nume}</p>
                     <p className="loc--selectat">Loc selectat: {numarScaunSelectat}</p>
                     <button type="submit" className="rez--button" onClick={onSubmit}>Adaugare rezervare</button>
                 </div>
 
         </div>
+
 
         <div className="lista--scaune--container">
             {listaScaune}
