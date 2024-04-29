2974e0594108749a2db9027a9d468074cc7da886
Tudor-Bondoc <tudor.bondoc2012@gmail.com>
2024-04-29 18:46:49 +0300

    finished cautare curse

--- client_admin/src/pages/Curse.jsx
+++ client_admin/src/pages/Curse.jsx
@@ -2,15 +2,73 @@ import React from "react";
 import axios from "axios";
 import Cursa from "../components/Cursa";
 import Add from "../../images/add.png"
+import "../styles/Curse.css"
 import { Link } from "react-router-dom"
+import {Formik, Form, Field, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
+import { format } from 'date-fns'
 
 export default function Curse() {
 
     const [listaCurse, setListaCurse] = React.useState([])
     const [listaTrasee, setListaTrasee] = React.useState([])
     const [listaAutocare, setListaAutocare] = React.useState([])
+    const [listaCurseFiltrata, setListaCurseFiltrata] = React.useState([])
 
-    const lista = listaCurse.map( (value, key) => {
+    const [show, setShow] = React.useState(false)
+
+    const listaOrasePornire = []
+    const listaOraseSosire = []
+
+    //Request catre API pentru lista de curse, trasee si autocare
+    React.useEffect(()=> {
+
+        axios.get("http://localhost:3002/curse").then((response)=> {
+            setListaCurse(response.data)
+        })
+
+        axios.get("http://localhost:3002/trasee").then((response)=> {
+            setListaTrasee(response.data)
+        })
+
+        axios.get("http://localhost:3002/autocare").then((response)=> {
+            setListaAutocare(response.data)
+        })
+
+    }, [])
+
+    function onSubmit(values) {
+
+        const orasPlecare = values.oras_pornire.toLowerCase();
+        const orasDestinatie = values.oras_sosire.toLowerCase();
+        let dataPlecare = 0
+        if (values.data_plecare!='')
+            dataPlecare = format(new Date(values.data_plecare), 'M/d/yyyy')
+
+        const listaFiltrata = listaCurse.filter(cursa => {
+
+            const traseuCursa = listaTrasee.find(traseu => traseu.id === cursa.TraseuID);
+            if (!traseuCursa) {
+                return false; // Dacă nu există traseu pentru cursă, o excludem
+            }
+            const dataPlecareCursa = format(new Date(cursa.zi_plecare), 'M/d/yyyy');
+
+            if (dataPlecare!=0) {
+                return traseuCursa.oras_pornire.toLowerCase() === orasPlecare &&
+                    traseuCursa.oras_sosire.toLowerCase() === orasDestinatie &&
+                    dataPlecare === dataPlecareCursa;
+            }
+            return traseuCursa.oras_pornire.toLowerCase() === orasPlecare &&
+                   traseuCursa.oras_sosire.toLowerCase() === orasDestinatie
+        })
+
+        setListaCurseFiltrata(listaFiltrata)
+        setShow(true)
+
+    }
+
+
+    const lista = listaCurseFiltrata.map( (value, key) => {
 
         const traseuSelectat = listaTrasee.find(traseu => traseu.id === value.TraseuID)
         if (!traseuSelectat) {
@@ -37,26 +95,79 @@ export default function Curse() {
         )
     })
 
-    //Request catre API pentru lista de curse, trasee si autocare
-    React.useEffect(()=> {
+    const validationSchema = Yup.object().shape({
+        oras_pornire: Yup.string().required('!'),
+        oras_sosire: Yup.string().required('!'),
+        data_plecare: Yup.date()
+    })
 
-        axios.get("http://localhost:3002/curse").then((response)=> {
-            setListaCurse(response.data)
-        })
+    const initialValues = {
+        oras_pornire: 'Cluj',
+        oras_sosire: 'Bucuresti',
+        data_plecare: ''
+    }
 
-        axios.get("http://localhost:3002/trasee").then((response)=> {
-            setListaTrasee(response.data)
-        })
-
-        axios.get("http://localhost:3002/autocare").then((response)=> {
-            setListaAutocare(response.data)
-        })
-
-    }, [])
+    for (const traseu of listaTrasee) {
+        if (!listaOrasePornire.includes(traseu.oras_pornire)) {
+            listaOrasePornire.push(traseu.oras_pornire)
+        }
+        if (!listaOraseSosire.includes(traseu.oras_sosire)) {
+            listaOraseSosire.push(traseu.oras_sosire)
+        }
+    }
 
     return(
         <div>
-            {lista}
+            <div className="cautare--curse">
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={onSubmit}
+                >
+                    <Form className="cautare--curse--form">
+
+                        <Field className="form--select" as="select" id="input--oras--pornire" name="oras_pornire" placeholder = "Oras pornire">
+                            {listaOrasePornire.map(oras => (
+                                <option key={oras} value={oras}>
+                                    {oras}
+                                </option>
+                            ))}
+                        </Field>
+                        <ErrorMessage name="oras_pornire" component="span" />
+
+                        <Field className="form--select" as="select" id="input--oras--destinatie" name="oras_sosire" placeholder = "Oras destinatie">
+                            {listaOraseSosire.map(oras => (
+                                <option key={oras} value={oras}>
+                                    {oras}
+                                </option>
+                            ))}
+                        </Field>
+                        <ErrorMessage name="oras_sosire" component="span" />
+
+                        <Field
+                            type = "date"
+                            name = "data_plecare"
+                            placeholder = "Data plecare"
+                            className = "form--select"
+                        />
+                        <ErrorMessage name="data_plecare" component="span" />
+                    
+                        <button type="submit" className="home--button">Cauta curse</button>
+
+                    </Form>
+
+                </Formik>
+            </div>
+
+            {show && <div className="curse--container">
+                        {lista && lista.length > 0 ? (
+                            lista
+                        ) : (
+                            <h1 className="curse--negasite">Nu au fost gasite curse</h1>
+                        )}
+                     </div>
+            }
+
             <Link to="/adaugare_cursa">
                 <img src={Add} className="add-button" />
             </Link>
