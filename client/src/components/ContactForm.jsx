92538d5dbe276ebac83e2fd8ff05c9d8718999f9
Tudor-Bondoc <tudor.bondoc2012@gmail.com>
2024-06-09 19:39:52 +0300

    minor changes styling

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -23,6 +23,8 @@ export default function ContactForm() {
     }
 
     return(
+
+        <div className="blue--wrapper">
         <div className="contact--form--container">
         <p className="contact--form--info">Aveti vreo sugestie sau nemultumire? Contactati-ne!</p>
         <Formik
@@ -58,6 +60,14 @@ export default function ContactForm() {
                 <button type="submit" className="contact--form--button">Trimite</button> 
             </Form>
         </Formik>
+
+        {/*<div className="custom-shape-divider-top-1717949562">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" className="shape-fill"></path>
+            </svg>
+        </div>*/}
+
+        </div>
         </div>
     )
 }
